f4deed792afffcfd00e6b83d87701b58275a39b0
wagnermoraisWarren <wagner.morais@warren.com.br>
2023-01-25 17:28:56 -0300

    importações de novas dependencias

--- src/main.js
+++ src/main.js
@@ -5,8 +5,29 @@ import { createServer, Model } from 'miragejs'
 
 import VueMask from 'v-mask';
 import JwPagination from 'jw-vue-pagination';
+import Vuelidate from 'vuelidate';
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+
 Vue.use(VueMask)
 Vue.component('jw-pagination', JwPagination);
+Vue.use(Vuelidate)
+Vue.use(Toast, {
+  transition: "Vue-Toastification__fade",
+  maxToasts: 3,
+  newestOnTop: true,
+  filterBeforeCreate: (toast, toasts) => {
+    if (toasts.filter(
+      t => t.type === toast.type
+    ).length !== 0) {
+      // Returning false discards the toast
+      return false;
+    }
+    // You can modify the toast if you want
+    return toast;
+  }
+});
 
 createServer({
   models: {
